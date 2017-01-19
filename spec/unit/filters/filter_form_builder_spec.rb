unudeveloper/BEforElectricalShop

Update filters to work with custom Ransackers
Fixes issues created when converting from metasearch.

Adds a custom ransacker to the rails_template to demo its interaction with filters.

[fixes #3510]

spec/unit/filters/filter_form_builder_spec.rb
@@ -4,6 +4,9 @@ class Post
   ransacker :custom_searcher do
     # nothing to see here
   end
+  ransacker :custom_searcher_numeric, type: :numeric do
+    # nothing to see here
+  end
 end
 
 describe ActiveAdmin::Filters::ViewHelper do
@@ -403,14 +406,22 @@ describe ActiveAdmin::Filters::ViewHelper do
 
   describe "custom search methods" do
 
+    it "should use the default type of the ransacker" do
+      body = Capybara.string(filter :custom_searcher_numeric)
+      expect(body).to have_selector("option[value=custom_searcher_numeric_equals]")
+      expect(body).to have_selector("option[value=custom_searcher_numeric_greater_than]")
+      expect(body).to have_selector("option[value=custom_searcher_numeric_less_than]")
+    end
+
     it "should work as select" do
       body = Capybara.string(filter :custom_searcher, as: :select, collection: ['foo'])
-      expect(body).to have_selector("select[name='q[custom_searcher]']")
+      expect(body).to have_selector("select[name='q[custom_searcher_eq]']")
     end
 
     it "should work as string" do
       body = Capybara.string(filter :custom_searcher, as: :string)
-      expect(body).to have_selector("input[name='q[custom_searcher]']")
+      expect(body).to have_selector("option[value=custom_searcher_contains]")
+      expect(body).to have_selector("option[value=custom_searcher_starts_with]")
     end
   end
 

Fivell/activeadmin

spec/unit/filters/filter_form_builder_spec.rb
@@ -7,6 +7,9 @@ class Post
   ransacker :custom_created_at_searcher do |parent|
     parent.table[:created_at]
   end
+  ransacker :custom_searcher_numeric, type: :numeric do
+    # nothing to see here
+  end
 end
 
 describe ActiveAdmin::Filters::ViewHelper do
@@ -406,16 +409,24 @@ describe ActiveAdmin::Filters::ViewHelper do
 
   describe "custom search methods" do
 
+    it "should use the default type of the ransacker" do
+      body = Capybara.string(filter :custom_searcher_numeric)
+      expect(body).to have_selector("option[value=custom_searcher_numeric_equals]")
+      expect(body).to have_selector("option[value=custom_searcher_numeric_greater_than]")
+      expect(body).to have_selector("option[value=custom_searcher_numeric_less_than]")
+    end
+
     it "should work as select" do
       body = Capybara.string(filter :custom_title_searcher, as: :select, collection: ['foo'])
-      expect(body).to have_selector("select[name='q[custom_title_searcher]']")
+      expect(body).to have_selector("select[name='q[custom_title_searcher_eq]']")
     end
 
     it "should work as string" do
       body = Capybara.string(filter :custom_title_searcher, as: :string)
-      expect(body).to have_selector("input[name='q[custom_title_searcher]']")
+      expect(body).to have_selector("option[value=custom_title_searcher_contains]")
+      expect(body).to have_selector("option[value=custom_title_searcher_starts_with]")
     end
-
+    
     describe "custom date range search" do
       let(:qteq) { "2010-10-01" }
       let(:lteq) { "2010-10-02" }