bcfe1fe1c6de8e31bd561842f61a57fe21eebfcc
Igor Fedoronchuk <fedoronchuk@gmail.com>
2017-01-30 19:28:01 +0200

    use activeadmin RouteBuilder for default form path, fixes #4544

--- lib/active_admin/views/pages/form.rb
+++ lib/active_admin/views/pages/form.rb
@@ -36,7 +36,7 @@ module ActiveAdmin
         end
 
         def default_form_path
-          resource.persisted? ? resource_path(resource) : collection_path
+          resource.persisted? ? active_admin_config.route_instance_path(resource, params) : active_admin_config.route_collection_path(params)
         end
 
         def default_form_config
